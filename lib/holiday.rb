da06a9574d9ce1c00e5f472f5d4aa71629a95a58
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-06-18 23:38:23 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -28,9 +28,9 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   holiday_hash.each do |season, holiday|
     if season == :winter
     holiday.each do |item|
-      if 
+      if
     end
-    
+
   end
 end
 
