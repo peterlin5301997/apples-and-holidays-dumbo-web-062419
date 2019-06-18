caa73de1862b769ff799185ff1c28695e43d8c48
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-06-18 23:52:34 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -26,7 +26,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
   holiday_hash.each do |season, holidays|
-    holidays.each do |holiday, supply|
+    holidays.each do |holiday|
       holiday_hash[:winter][holiday].push("Balloons")
   end
 end
