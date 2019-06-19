1e3f47727adf87242c79040a2b0c17c9061348e9
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-06-19 00:03:34 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -33,8 +33,8 @@ end
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-  holiday_hash[:spring][:memorial_day].push("Grill")
-  holiday_hash[:spring][:memorial_day].push("Table Cloth")
+  holiday_hash[:spring][:memorial_day].push(supply)
+  holiday_hash[:spring][:memorial_day].push(supply)
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
