cd151bd0d51c6c802299e00a9f5e2a2055ef66de
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-06-19 00:48:05 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -80,5 +80,13 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  array = []
+  holiday_hash.each do |season, holidays|
+    holidays.each do |holiday, supplies|
+      supplies.each do |supply|
+        if supply == "BBQ"
+          array.push(holiday)
+      end
+    end
+  end
 end
