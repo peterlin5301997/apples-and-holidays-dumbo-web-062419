e0550f7563240c4172e3b9a146ce91efccaab4e8
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-06-19 00:41:27 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays|
     puts "#{season.capitalize}:"
     holidays.each do |holiday, supplies|
-      holiday.to_s.split("_")
+      arrHoliday = holiday.to_s.split("_")
 
       strSupplies = ""
       supplies.each do |supply|
