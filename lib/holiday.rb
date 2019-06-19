71ea6bd75ce9f96bcd90cd79af4db80c27e3b1df
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-06-19 00:05:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -45,7 +45,7 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-  return holiday_hash[:winter].values
+  return holiday_hash[:winter].values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
