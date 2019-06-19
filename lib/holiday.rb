873920af02498babb7379c7219420c43c5320b43
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-06-19 00:30:31 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,7 +74,7 @@ def all_supplies_in_holidays(holiday_hash)
           strSupplies += supply
         end
       end
-      puts "  #{holiday}: #{strSupplies}"
+      puts "  #{strHoliday}: #{strSupplies}"
     end
   end
 end
