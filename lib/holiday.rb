7a1f9e92f9a89c1c8b32760f57931351d1dc858a
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-06-19 00:09:10 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,7 +56,12 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each do |season, holidays|
+    puts "#{season}:"
+    holidays.each do |holiday, supply|
+      puts "  #{holiday}: #{supply}"
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
